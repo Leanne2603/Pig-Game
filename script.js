0017d6af3281b52d56182cb00012f074cf02fa19
Leanne <leannestranks86@hotmail.com>
2020-12-11 19:12:36 +1000

    Created const for elements and created a hidden id for dice class

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+// Retrieving the IDs to select elements
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1");
+const diceEl = document.querySelector(".dice")
+
+// Sets the player scores to zero
+score0El.textContent = 0
+score1El.textContent = 0
+
+// Hides the dice in the initial load before dice is rolled
+diceEl.classList.add("hidden")
+
+
